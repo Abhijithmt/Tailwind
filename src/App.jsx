619a6fe23ed4f1665ad790745ef410c1b767d11f
abhijith <abhijithmtmkm@gmail.com>
2026-02-10 16:29:17 +0530

    learning tailwind

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Tailwind from './tailwind'
+function App() {
+  return (
+    <Tailwind/>
+  )
+}
+
+export default App
---
+++ src/tailwind.jsx
@@ -0,0 +1,12 @@
+
+function Tailwind(){
+    return (
+        <>
+        <h1 className="text-6xl text-yellow-300 bg-black py-5 px-3 textal text-center font-bold font-mono font ">Tailwind studying Day1</h1>
+         <div className="flex flex-row items-center justify-center mt-3 gap-10">
+        <button className="px-8 py-5 bg-red-600 text-white rounded-[10px] hover:bg-black">Explore More</button>
+        </div>
+        </>
+    )
+}
+export default Tailwind
