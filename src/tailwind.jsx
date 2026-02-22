bfe8e7c9a35c64a2b0d87efcd88d1fd0afd56899
abhijith <abhijithmtmkm@gmail.com>
2026-02-22 22:54:26 +0530

    tailwind study

--- src/tailwind.jsx
+++ src/tailwind.jsx
@@ -8,7 +8,7 @@ function Tailwind(){
         </div>
         <div className='px-2.5 py-2.5 border max-w-2xs shadow-2xl ml-5 flex flex-col items-center'>
             <img className='rounded-[10px] h-24 px-6 ' src={photo} alt="myphoto" />
-            <h2 className='text-center'>ABHIJITH</h2>
+            <h2 className='text-center text-purple-950 font-bold'>ABHIJITH</h2>
             <p className='text-gray-500 font-mono text-xs'>fullstack developer</p>
 
         </div>
